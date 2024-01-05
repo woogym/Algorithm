f517b68ecc6888ffc95eff2d51a93cddc95e1895
woogym <kwj9294@gmail.com>
2024-01-06 02:15:24 +0900

    24.01.06

--- Backjoon/ch01_사칙연산/B1001.java
+++ Backjoon/ch01_사칙연산/B1001.java
@@ -4,8 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-public class B1000 {
+public class B1001 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -13,6 +12,6 @@ public class B1000 {
         int a = Integer.parseInt(input[0]);
         int b = Integer.parseInt(input[1]);
 
-        System.out.println(a + b);
+        System.out.println(a - b);
     }
 }
