c6e0de3207821615dfcfd2a3243da654f5db9308
woogym <kwj9294@gmail.com>
2024-01-08 23:04:00 +0900

    24.01.08

---
+++ Backjoon/ch02_조건문/B25304.java
@@ -0,0 +1,31 @@
+package Algorithm.Backjoon.ch02_조건문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B25304 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int allPrice = Integer.parseInt(br.readLine());
+        int productCount = Integer.parseInt((br.readLine()));
+
+        int [] price = new int [productCount];
+        int [] count = new int [productCount];
+        int sum = 0;
+
+        for (int i=0; i < productCount; i++) {
+            String [] inputs = br.readLine().split(" ");
+            price[i] = Integer.parseInt(inputs[0]);
+            count [i] = Integer.parseInt(inputs[1]);
+            sum += price[i] * count[i];
+        }
+
+        if(sum == allPrice) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
