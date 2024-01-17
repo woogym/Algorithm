92fc2005b1050c3a12197b4340cfe4d6bcbf2fab
woogym <kwj9294@gmail.com>
2024-01-17 12:56:50 +0900

    24.01.17

---
+++ Backjoon/ch03_반복문/B15552.java
@@ -0,0 +1,25 @@
+package Algorithm.Backjoon.ch03_반복문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i =0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+            bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+        }
+
+        bw.close();
+    }
+}
