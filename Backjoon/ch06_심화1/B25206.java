ae6845023ef728b617a0b5febf9e7498d2c067d4
woogym <kwj9294@gmail.com>
2024-03-18 14:03:05 +0900

    24.03.18

---
+++ Backjoon/ch06_심화1/B25206.java
@@ -0,0 +1,14 @@
+package Algorithm.Backjoon.ch06_심화1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B25206 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        // 귀찮아서 다음에..
+    }
+}
