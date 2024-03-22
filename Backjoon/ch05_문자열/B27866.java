5ba33c294b49f5e5c6f7112c899419af4169600c
woogym <kwj9294@gmail.com>
2024-03-23 01:43:24 +0900

    24.03.23

---
+++ Backjoon/ch05_문자열/B27866.java
@@ -0,0 +1,16 @@
+package Algorithm.Backjoon.ch05_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B27866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println(str.charAt(num - 1));
+    }
+}
