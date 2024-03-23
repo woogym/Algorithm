00cf0c314f7ca2bef0ba908b730b121b5676adcf
woogym <kwj9294@gmail.com>
2024-03-24 01:46:18 +0900

    24.03.24

---
+++ Backjoon/ch05_문자열/B2743.java
@@ -0,0 +1,15 @@
+package Algorithm.Backjoon.ch05_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(str.length());
+    }
+}
