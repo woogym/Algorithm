0302235ee20c9b77836968a66195d5a891331b1e
woogym <kwj9294@gmail.com>
2024-03-29 17:33:58 +0900

    24.03.29

---
+++ Backjoon/ch05_문자열/B1152.java
@@ -0,0 +1,15 @@
+package Algorithm.Backjoon.ch05_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        System.out.print(st.countTokens());
+    }
+}
