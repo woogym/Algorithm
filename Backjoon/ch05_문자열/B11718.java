803a4d67469d3d7ffa81b3e5443e683439801bc9
woogym <kwj9294@gmail.com>
2024-04-05 00:28:05 +0900

    24.04.05

---
+++ Backjoon/ch05_문자열/B11718.java
@@ -0,0 +1,19 @@
+package Algorithm.Backjoon.ch05_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+
+        while ((str=br.readLine()) != null) {
+            System.out.println(str);
+        }
+
+        br.close();
+    }
+}
