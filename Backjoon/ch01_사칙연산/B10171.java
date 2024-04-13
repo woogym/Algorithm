96b6b802d02f1336f90f4cc6aaf2653424f7bc13
woogym <kwj9294@gmail.com>
2024-04-13 11:22:19 +0900

    24.04.13

---
+++ Backjoon/ch01_사칙연산/B10171.java
@@ -0,0 +1,11 @@
+package Algorithm.Backjoon.ch01_사칙연산;
+
+public class B10171 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|\n");
+    }
+}
