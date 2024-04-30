801c3a9c06477dbf449c103f5405a62676eaab6c
woogym <kwj9294@gmail.com>
2024-05-01 00:02:52 +0900

    24.05.01

---
+++ Programers/코딩테스트_기초/Solution.java
@@ -0,0 +1,12 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+import java.math.BigInteger;
+
+// 두 수의 합
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+
+        return answer;
+    }
+}
