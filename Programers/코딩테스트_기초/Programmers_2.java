6363c76d6f2f2baa5937ff0db10b725ed98dfff8
woogym <kwj9294@gmail.com>
2024-05-02 00:11:09 +0900

    24.05.02

---
+++ Programers/코딩테스트_기초/Programmers_2.java
@@ -0,0 +1,18 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+public class Programmers_2 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer;
+
+            if ((-50000 <= num1 && num1 <= 50000) && (-50000 <= num2 && num2 <= 50000)) {
+                answer = num1 - num2;
+            } else {
+                answer = 1;
+            }
+
+            return answer;
+        }
+    }
+}
