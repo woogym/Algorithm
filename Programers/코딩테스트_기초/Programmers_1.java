99cff8b0c2378a132e1341d24c514987ee050ac1
woogym <kwj9294@gmail.com>
2024-05-03 00:12:18 +0900

    modify class architecture

--- Programers/코딩테스트_기초/Programmers_1.java
+++ Programers/코딩테스트_기초/Programmers_1.java
@@ -1,12 +1,13 @@
 package Algorithm.Programers.코딩테스트_기초;
 
-import java.math.BigInteger;
 
-// 두 수의 합
-class Solution {
-    public int solution(int num1, int num2) {
-        int answer = num1 + num2;
+public class Programmers_1 {
 
-        return answer;
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = num1 + num2;
+
+            return answer;
+        }
     }
 }
