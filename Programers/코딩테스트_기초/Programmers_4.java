d4ac312688ebe7070fa9a616c83f85d091288c74
woogym <kwj9294@gmail.com>
2024-05-15 23:57:14 +0900

    24.05.14

---
+++ Programers/코딩테스트_기초/Programmers_4.java
@@ -0,0 +1,20 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+public class Programmers_4 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+            boolean num1Boolean1 = 0 <= num1;
+            boolean num1Boolean2 = num1 <= 100;
+            boolean num2Boolean1 = 0 <= num2;
+            boolean num2Boolean2 = num2 <= 100;
+
+            if (num1Boolean1 && num1Boolean2 && num2Boolean1 && num2Boolean2) {
+                answer += num1 / num2;
+            }
+
+            return answer;
+        }
+    }
+}
