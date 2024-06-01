6153e1e11a0ede04ebae9995d5d100ba54f882d3
woogym <kwj9294@gmail.com>
2024-06-01 15:47:42 +0900

    24.06.01

---
+++ Programers/코딩테스트_입문/Programmers_5.java
@@ -0,0 +1,14 @@
+package Algorithm.Programers.코딩테스트_입문;
+
+public class Programmers_5 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            if (num1 == num2) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+}
