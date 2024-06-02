f7a02f3721ed857c0762218917d62e33f2e403cb
woogym <kwj9294@gmail.com>
2024-06-02 16:06:56 +0900

    24.06.02

--- Programers/코딩테스트_입문/Programmers_6.java
+++ Programers/코딩테스트_입문/Programmers_6.java
@@ -1,2 +1,26 @@
-package Algorithm.Programers.코딩테스트_입문;public class Programmers_6 {
+package Algorithm.Programers.코딩테스트_입문;
+
+// 나머지 구하기
+public class Programmers_6 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+
+            if (num1 <= 100 && num2 <= 100)
+                answer = num1 % num2;
+
+            return answer;
+        }
+    }
+}
+
+// 이런식으로도 풀 수 있을거 같다
+class Solution {
+    public int solution(int num1, int num2) {
+        while (num1 >= num2) {
+            num1 = num1 - num2;
+        }
+        int answer = num1;
+        return answer;
+    }
 }
