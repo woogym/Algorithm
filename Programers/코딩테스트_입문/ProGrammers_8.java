c232e83b52a63928886b734c4729c5f51689857b
woogym <kwj9294@gmail.com>
2024-06-08 04:21:18 +0900

    24.06.08

--- Programers/코딩테스트_입문/ProGrammers_8.java
+++ Programers/코딩테스트_입문/ProGrammers_8.java
@@ -1,2 +1,21 @@
-package Algorithm.Programers.코딩테스트_입문;public class ProGrammers_8 {
+package Algorithm.Programers.코딩테스트_입문;
+
+import java.util.Arrays;
+
+public class ProGrammers_8 {
+    class Solution {
+        public double solution(int[] numbers) {
+            double answer;
+            double all = 0;
+            int arySize = numbers.length;
+
+            for (int i = 0; i < arySize; i++) {
+                all += numbers[i];
+            }
+
+            answer = all / arySize;
+
+            return answer;
+        }
+    }
 }
