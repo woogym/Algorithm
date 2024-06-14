e3f402bca3a12715fe4c69299a39df6eb7d9626d
woogym <kwj9294@gmail.com>
2024-06-14 12:49:40 +0900

    24.06.14

---
+++ Programers/코딩테스트_입문/Programmers_9.java
@@ -0,0 +1,22 @@
+package Algorithm.Programers.코딩테스트_입문;
+
+public class Programmers_9 {
+
+    class Solution {
+        public int solution(int angle) {
+            int result = 0;
+
+            if (angle < 90) {
+                result = 1;
+            } else if (angle == 90) {
+                result = 2;
+            } else if (angle < 180) {
+                result = 3;
+            } else if (angle == 180) {
+                result = 4;
+            }
+
+            return result;
+        }
+    }
+}
