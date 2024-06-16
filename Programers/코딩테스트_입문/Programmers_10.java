600dd35a55c88ba299e311383c63b7141f57c214
woogym <kwj9294@gmail.com>
2024-06-16 13:41:57 +0900

    24.06.16

--- Programers/코딩테스트_입문/Programmers_10.java
+++ Programers/코딩테스트_입문/Programmers_10.java
@@ -16,5 +16,18 @@ public class Programmers_10 {
 
             return answer;
         }
+
+        // 또 다른 풀이1
+        // 매개변수로 들어오는 원본 값에 대한 변형이 이루어지지 않는다,
+        // 따라서 다른곳에서 해당 메소드가 활용될때 해당 메소드의 인자값으로 들어온 값은 변형이 이루어지지 않는다
+        public int solution1(int number) {
+            int answer = 0;
+
+            for (int i = 2; i <= number; i += 2) {
+                answer += i;
+            }
+
+            return answer;
+        }
     }
 }
