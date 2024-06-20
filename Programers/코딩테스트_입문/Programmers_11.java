f3189549a6f3d885f99ec949316aac6939aac809
woogym <kwj9294@gmail.com>
2024-06-20 14:36:20 +0900

    24.06.20

--- Programers/코딩테스트_입문/Programmers_11.java
+++ Programers/코딩테스트_입문/Programmers_11.java
@@ -2,6 +2,7 @@ package Algorithm.Programers.코딩테스트_입문;
 
 public class Programmers_11 {
     class Solution {
+
         public int solution(int num1, int num2) {
 
             double division = (double) num1 / num2;
@@ -9,6 +10,13 @@ public class Programmers_11 {
             double result = division * 1000;
 
             return (int) result;
+        }
+
+        // 이런 방법이.. 수학적 사고..
+        public int solution1(int num1, int num2) {
+            int answer = 0;
+
+            return num1 * 1000 / num2;
         }
     }
 }
