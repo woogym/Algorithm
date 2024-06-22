b71ec38359b3bb83685e9d24caa7410f9cdc831d
woogym <kwj9294@gmail.com>
2024-06-22 13:15:55 +0900

    24.06.22

---
+++ Programers/코딩테스트_입문/Programmers_12.java
@@ -0,0 +1,23 @@
+package Algorithm.Programers.코딩테스트_입문;
+
+// 양꼬치
+public class Programmers_12 {
+    class Solution {
+        public int solution(int n, int k) {
+            final int yangCcochiPrice = 12000;
+            final int drinkPrice = 2000;
+
+            int saleCount = n / 10;
+            int salePrice = 0;
+            int answer;
+
+            if (saleCount >= 1) {
+                salePrice = saleCount * drinkPrice;
+            }
+
+            answer = (yangCcochiPrice * n) + (drinkPrice * k) - salePrice;
+
+            return answer;
+        }
+    }
+}
