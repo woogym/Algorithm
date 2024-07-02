a1af674849450f52464e9711be4f194c711be6b6
woogym <kwj9294@gmail.com>
2024-07-02 14:41:16 +0900

    24.07.02

--- Programers/코딩테스트_입문/Programmers_14.java
+++ Programers/코딩테스트_입문/Programmers_14.java
@@ -1,2 +1,13 @@
-package Algorithm.Programers.코딩테스트_입문;public class Programmers_14 {
+package Algorithm.Programers.코딩테스트_입문;
+
+public class Programmers_14 {
+    class Solution {
+        public int solution(String message) {
+
+            // 문자의 앞 뒤 공백을 제거해주는 trim을 기억하며
+            int answer = message.length() * 2;
+
+            return answer;
+        }
+    }
 }
