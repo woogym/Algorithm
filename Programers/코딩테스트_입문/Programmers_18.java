c3dae8eed0b373287eaafcddfff3a8fe26254ff5
woogym <kwj9294@gmail.com>
2024-07-06 12:16:50 +0900

    24.07.06

---
+++ Programers/코딩테스트_입문/Programmers_18.java
@@ -0,0 +1,25 @@
+package Algorithm.Programers.코딩테스트_입문;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Programmers_18 {
+    class Solution {
+        public int solution(int[] array, int n) {
+            int answer = 0;
+
+            for (int num : array) {
+                if (num == n) {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+
+        // stream 사용에 익숙해지기
+        public int solution1(int[] array, int n) {
+            return (int) Arrays.stream(array).filter(i -> i == n).count();
+        }
+    }
+}
