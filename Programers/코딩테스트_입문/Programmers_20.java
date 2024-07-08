a1c890255aa994b608f188ef5eed094f3fda49f5
woogym <kwj9294@gmail.com>
2024-07-08 12:52:23 +0900

    24.07.08

---
+++ Programers/코딩테스트_입문/Programmers_20.java
@@ -0,0 +1,22 @@
+package Algorithm.Programers.코딩테스트_입문;
+
+import java.util.Arrays;
+
+public class Programmers_20 {
+    class Solution {
+        public int[] solution(int[] numbers) {
+            int[] answer = new int[numbers.length];
+
+            for (int i = 0; i < numbers.length; i++) {
+                answer[i] = numbers[i] * 2;
+            }
+
+            return answer;
+        }
+
+        // 이런식으로 stream으로 풀 수 있음
+        public int[] solution2(int[] numbers) {
+            return Arrays.stream(numbers).map(i -> i * 2).toArray();
+        }
+    }
+}
