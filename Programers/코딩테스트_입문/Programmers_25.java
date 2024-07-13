cb48493756584aad1d391d8a436aefc7443d31b0
woogym <kwj9294@gmail.com>
2024-07-13 21:48:00 +0900

    24.07.13

---
+++ Programers/코딩테스트_입문/Programmers_25.java
@@ -0,0 +1,15 @@
+package Algorithm.Programers.코딩테스트_입문;
+
+public class Programmers_25 {
+    class Solution {
+        public int[] solution(int money) {
+            int[] answer = new int[2];
+            final int americano = 5500;
+
+            answer[0] = money / americano;
+            answer[1] = money % americano;
+
+            return answer;
+        }
+    }
+}
