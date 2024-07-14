44c0e8d73025625fb8665fa21dffd7738f36b060
woogym <kwj9294@gmail.com>
2024-07-14 22:01:52 +0900

    24.07.14

---
+++ Programers/코딩테스트_입문/Programmers_26.java
@@ -0,0 +1,18 @@
+package Algorithm.Programers.코딩테스트_입문;
+
+
+// 배열 자르기
+public class Programmers_26 {
+    class Solution {
+        public int[] solution(int[] numbers, int num1, int num2) {
+            int[] answer = new int[num2 - num1 + 1];
+            int k = 0;
+
+            for (int i = num1; i <= num2; i++) {
+                answer[k++] = numbers[i];
+            }
+
+            return answer;
+        }
+    }
+}
