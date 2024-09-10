541750321cfaac33b5d6383a774de958190455db
woogym <kwj9294@gmail.com>
2024-09-11 04:51:12 +0900

    24.09.11

---
+++ Programers/코딩테스트_입문/part_81to100/Programmers_82.java
@@ -0,0 +1,10 @@
+package Algorithm.Programers.코딩테스트_입문.part_81to100;
+
+// 종이 자르기
+public class Programmers_82 {
+    class Solution {
+        public int solution(int M, int N) {
+            return M * N - 1;
+        }
+    }
+}
