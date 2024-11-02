a970c35917c46d1076f271f5b68fbfdfa7d20931
woogym <kwj9294@gmail.com>
2024-11-02 18:12:12 +0900

    문자열 붙여서 출력하기

---
+++ Programers/코딩테스트_기초/Programmers_3.java
@@ -0,0 +1,15 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+import java.util.Scanner;
+
+public class Programmers_3 {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            String b = sc.next();
+
+            System.out.println(a.trim() + b.trim());
+        }
+    }
+}
