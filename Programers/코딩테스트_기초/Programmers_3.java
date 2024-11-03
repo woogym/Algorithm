45f114f4672f9639d2bb8c4d7ea530e22db34507
woogym <kwj9294@gmail.com>
2024-11-03 18:14:36 +0900

    문자열 정수로 변환하기

--- Programers/코딩테스트_기초/Programmers_3.java
+++ Programers/코딩테스트_기초/Programmers_3.java
@@ -2,6 +2,7 @@ package Algorithm.Programers.코딩테스트_기초;
 
 import java.util.Scanner;
 
+// 문자여 붙여서 출력하기
 public class Programmers_3 {
     public class Solution {
         public static void main(String[] args) {
---
+++ Programers/코딩테스트_기초/Programmers_4.java
@@ -0,0 +1,11 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+public class Programmers_4 {
+    class Solution {
+        public int solution(String n_str) {
+            int answer = Integer.parseInt(n_str);
+
+            return answer;
+        }
+    }
+}
