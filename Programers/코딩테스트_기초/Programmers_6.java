a31aa45635662e95498ebde7bc0d01172b3ae87f
woogym <kwj9294@gmail.com>
2024-11-18 22:45:20 +0900

    정수 부분

---
+++ Programers/코딩테스트_기초/Programmers_6.java
@@ -0,0 +1,10 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+// 정수 부분
+public class Programmers_6 {
+    class Solution {
+        public int solution(double flo) {
+            return (int) Math.floor(flo);
+        }
+    }
+}
