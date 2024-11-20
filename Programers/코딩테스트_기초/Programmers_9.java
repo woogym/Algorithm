10bdc5b343b561642fa07ec671a6267e6b2f15be
woogym <kwj9294@gmail.com>
2024-11-20 23:11:13 +0900

    rny_string

---
+++ Programers/코딩테스트_기초/Programmers_9.java
@@ -0,0 +1,10 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+// rny_string
+public class Programmers_9 {
+    class Solution {
+        public String solution(String rny_string) {
+            return rny_string.replaceAll("m", "rn");
+        }
+    }
+}
