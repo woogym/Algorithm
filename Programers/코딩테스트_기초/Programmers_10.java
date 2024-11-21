4caf7480ab5df7c32a9a0cf3f5e42de8b3109015
woogym <kwj9294@gmail.com>
2024-11-21 23:18:15 +0900

    공백으로 구분하기

---
+++ Programers/코딩테스트_기초/Programmers_10.java
@@ -0,0 +1,9 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+public class Programmers_10 {
+    class Solution {
+        public String[] solution(String my_string) {
+            return my_string.trim().split("\\s+"); // 하나 이상의 공백을 의미하는 정규식! / "[ ]+" 도 동일한 기능을 수행함
+        }
+    }
+}
