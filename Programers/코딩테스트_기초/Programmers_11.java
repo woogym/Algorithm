5e0b68c6ce3e07afbff94466532e4a04153f6db2
woogym <kwj9294@gmail.com>
2024-11-23 23:20:00 +0900

    공백으로 구분하기 1

---
+++ Programers/코딩테스트_기초/Programmers_11.java
@@ -0,0 +1,9 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+public class Programmers_11 {
+    class Solution {
+        public String[] solution(String my_string) {
+            return my_string.trim().split(" ");
+        }
+    }
+}
