9e3eb9b9bb089297e9259e5d015719aadccf934a
woogym <kwj9294@gmail.com>
2024-11-29 00:20:54 +0900

    배열에서 대소문자 변환하기

--- Programers/코딩테스트_기초/Programmers_14.java
+++ Programers/코딩테스트_기초/Programmers_14.java
@@ -1,6 +1,6 @@
 package Algorithm.Programers.코딩테스트_기초;
 
-// 배열에서 대소문자 변경하기
+// 배열에서 대소문자 변환하기
 public class Programmers_14 {
     class Solution {
         public String[] solution(String[] strArr) {
