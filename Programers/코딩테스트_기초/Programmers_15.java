279552fec49974ffb718217e2fcf6adc73398380
woogym <kwj9294@gmail.com>
2024-12-02 00:24:00 +0900

    소문자로 바꾸기

---
+++ Programers/코딩테스트_기초/Programmers_15.java
@@ -0,0 +1,10 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+// 소문자로 바꾸기
+public class Programmers_15 {
+    class Solution {
+        public String solution(String myString) {
+            return myString.toLowerCase();
+        }
+    }
+}
