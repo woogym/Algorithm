00136ff63661f8890fa401e030a30b070e60acb6
woogym <kwj9294@gmail.com>
2024-12-03 00:25:08 +0900

    대문자로 바꾸기

---
+++ Programers/코딩테스트_기초/Programmers_16.java
@@ -0,0 +1,11 @@
+package Algorithm.Programers.코딩테스트_기초;
+
+// 대문자로 바꾸기
+public class Programmers_16 {
+    class Solution {
+        public String solution(String myString) {
+            String answer = "";
+            return myString.toUpperCase();
+        }
+    }
+}
