68013bc3db77282c1556bfdc43e15bc8eba07ca4
woogym <kwj9294@gmail.com>
2024-12-10 04:09:11 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ Programers/level1/Programmers_5.java
@@ -0,0 +1,16 @@
+package Algorithm.Programers.level1;
+
+// x만큼 간격이 있는 n개의 숫자
+public class Programmers_5 {
+    class Solution {
+        public long[] solution(int x, int n) {
+            long[] answer = new long[n];
+
+            for (int i = 0; i < n; i++) {
+                answer[i] = (long) x * (i + 1);
+            }
+
+            return answer;
+        }
+    }
+}
