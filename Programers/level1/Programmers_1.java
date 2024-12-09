5166e4b1937d46519ddfe42a0a4fc8fd634aa228
woogym <kwj9294@gmail.com>
2024-12-10 03:52:15 +0900

    약수의 합

---
+++ Programers/level1/Programmers_1.java
@@ -0,0 +1,23 @@
+package Algorithm.Programers.level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 약수의 합
+public class Programmers_1 {
+    class Solution {
+        public int solution(int n) {
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    list.add(i);
+                }
+            }
+
+            return list.stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
+        }
+    }
+}
