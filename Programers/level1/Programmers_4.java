512fb4b62b0e83566c7aeae9b08081322e547856
woogym <kwj9294@gmail.com>
2024-12-10 04:05:01 +0900

    평균 구하기

---
+++ Programers/level1/Programmers_4.java
@@ -0,0 +1,23 @@
+package Algorithm.Programers.level1;
+
+import java.util.Arrays;
+
+// 평균 구하기
+public class Programmers_4 {
+    class Solution {
+        public double solution(int[] arr) {
+            int num = 0;
+
+            for (int i : arr) {
+                num += i;
+            }
+
+            return (double) num / arr.length;
+        }
+
+        // 존나 오래 걸리는 풀이..
+        public double solution1(int[] arr) {
+            return (double) Arrays.stream(arr).sum() / arr.length;
+        }
+    }
+}
