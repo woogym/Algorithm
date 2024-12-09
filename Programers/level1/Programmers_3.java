6a3ea6a8e5cc8f5d696b3372d472614b7f5bb9f6
woogym <kwj9294@gmail.com>
2024-12-10 04:00:01 +0900

    짝수와 홀수

---
+++ Programers/level1/Programmers_3.java
@@ -0,0 +1,15 @@
+package Algorithm.Programers.level1;
+
+// 짝수와 홀수
+public class Programmers_3 {
+    class Solution {
+        public String solution(int num) {
+
+            if (num % 2 == 0) {
+                return "Even";
+            }
+
+            return "Odd";
+        }
+    }
+}
