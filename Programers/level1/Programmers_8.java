921c820032a4e1e95d5475dd0185bbfe7d951f65
woogym <kwj9294@gmail.com>
2024-12-10 22:18:39 +0900

    두 정수 사이의 합

---
+++ Programers/level1/Programmers_8.java
@@ -0,0 +1,32 @@
+package Algorithm.Programers.level1;
+
+public class Programmers_8 {
+    class Solution {
+        public long solution(int a, int b) {
+            long answer = 0;
+
+            if (a <= b) {
+                for (int i = a; i <= b; i++) {
+                    answer += i;
+                }
+            } else {
+                for (int i = b; i <= a; i++) {
+                    answer += i;
+                }
+            }
+
+
+            return answer;
+        }
+
+        public long solution1(int a, int b) {
+            long answer = 0;
+
+            for (int i = Math.min(a,b); i < Math.max(a,b); i++) {
+                answer += i;
+            }
+
+            return answer;
+        }
+    }
+}
