4d0f7bbd0edf5e6d7e31c0eb4e911acc1fb0dc49
woogym <kwj9294@gmail.com>
2024-12-10 21:44:16 +0900

    문자열 정수로 바꾸기

--- Programers/level1/Programmers_7.java
+++ Programers/level1/Programmers_7.java
@@ -11,5 +11,10 @@ public class Programmers_7 {
 
             return Integer.parseInt(s);
         }
+
+        // 이렇게 해도 됨 -> parseInt는 산술 연산 부호까지 파싱해줌
+        public int solution1(String s) {
+            return Integer.parseInt(s);
+        }
     }
 }
