c49b819713348d55f84b1e037abb6191081c761a
woogym <kwj9294@gmail.com>
2024-12-18 23:32:24 +0900

    순서 바꾸기

---
+++ Programers/level1/Programmers_16.java
@@ -0,0 +1,21 @@
+package Algorithm.Programers.level1;
+
+// 순서 바꾸기
+public class Programmers_16 {
+    class Solution {
+        public int[] solution(int[] num_list, int n) {
+            int[] answer = new int[num_list.length];
+            int index = 0;
+
+            for (int i = n; i < num_list.length; i++) {
+                answer[index++] = num_list[i];
+            }
+
+            for (int i = 0; i < n; i++) {
+                answer[index++] = num_list[i];
+            }
+
+            return answer;
+        }
+    }
+}
