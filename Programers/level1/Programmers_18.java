fb2e16b2a339d252468ddc5ee503032dd782cf6c
woogym <kwj9294@gmail.com>
2024-12-19 23:43:53 +0900

    첫 번째로 나오는 음수

---
+++ Programers/level1/Programmers_18.java
@@ -0,0 +1,17 @@
+package Algorithm.Programers.level1;
+
+// 첫 번째로 나오는 음수
+public class Programmers_18 {
+    class Solution {
+        public int solution(int[] num_list) {
+            for (int i = 0; i < num_list.length; i++) {
+
+                if (num_list[i] < 0) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
