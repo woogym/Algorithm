9213812dd5de641a5ed9c152196160ea962fde2a
woogym <kwj9294@gmail.com>
2024-12-21 23:48:07 +0900

    카운트 다운

---
+++ Programers/level1/Programmers_19.java
@@ -0,0 +1,17 @@
+package Algorithm.Programers.level1;
+
+// 카운트 다운
+public class Programmers_19 {
+    class Solution {
+        public int[] solution(int start_num, int end_num) {
+            int[] answer = new int[start_num - end_num + 1];
+            int index = 0;
+
+            for (int i = start_num; end_num <= i; i--) {
+                answer[index++] = i;
+            }
+
+            return answer;
+        }
+    }
+}
