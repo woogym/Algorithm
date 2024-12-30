dcffbd06d59a5f7e4f7a1726d4f1f2d7c8e945d4
woogym <kwj9294@gmail.com>
2024-12-30 19:19:11 +0900

    p181920

---
+++ Programers/level0/P181920.java
@@ -0,0 +1,16 @@
+package Algorithm.Programers.level0;
+
+// 카운트 업
+public class P181920 {
+    class Solution {
+        public int[] solution(int start_num, int end_num) {
+            int[] answer = new int[end_num - start_num + 1];
+
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = start_num++;
+            }
+
+            return answer;
+        }
+    }
+}
