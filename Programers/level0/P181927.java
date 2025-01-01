110166fc076993823e41903df2e4de6678c82897
woogym <kwj9294@gmail.com>
2025-01-01 20:27:16 +0900

    p181927

---
+++ Programers/level0/P181927.java
@@ -0,0 +1,22 @@
+package Algorithm.Programers.level0;
+
+// 마지막 두 원소
+public class P181927 {
+    class Solution {
+        public int[] solution(int[] num_list) {
+            int[] answer = new int[num_list.length + 1];
+
+            for (int i = 0; i < num_list.length; i++) {
+                answer[i] = num_list[i];
+            }
+
+            if (num_list[num_list.length - 1] <= num_list[num_list.length - 2]) {
+                answer[answer.length - 1] = num_list[num_list.length - 1] * 2;
+            } else if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
+                answer[answer.length - 1] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
+            }
+
+            return answer;
+        }
+    }
+}
