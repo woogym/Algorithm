c27fc9fb86f620177446a0a4b89e6897b1eb58ab
woogym <kwj9294@gmail.com>
2025-01-03 22:25:14 +0900

    p181937

---
+++ Programers/level0/P181937.java
@@ -0,0 +1,14 @@
+package Algorithm.Programers.level0;
+
+public class P181937 {
+    class Solution {
+        public int solution(int num, int n) {
+
+            if (num % n == 0) {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
