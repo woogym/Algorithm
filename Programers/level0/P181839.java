31d7059326d54550465ef66d378ce7f535061470
woogym <kwj9294@gmail.com>
2025-01-05 22:35:52 +0900

    p181839

---
+++ Programers/level0/P181839.java
@@ -0,0 +1,16 @@
+package Algorithm.Programers.level0;
+
+public class P181839 {
+    class Solution {
+        public int solution(int a, int b) {
+
+            if (a % 2 == 1 && b % 2 == 1) {
+                return (a * a) + (b * b);
+            } else if (a % 2 == 1 || b % 2 == 1) {
+                return 2 * (a + b);
+            } else {
+                return Math.abs(a - b);
+            }
+        }
+    }
+}
