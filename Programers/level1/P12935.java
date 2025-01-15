cde6260e5dee081be99d1c8cd341893cf27e3385
woogym <kwj9294@gmail.com>
2025-01-15 20:11:40 +0900

    p12935

---
+++ Programers/level1/P12935.java
@@ -0,0 +1,46 @@
+package Algorithm.Programers.level1;
+
+import java.util.Arrays;
+
+public class P12935 {
+    class Solution {
+        public int[] solution(int[] arr) {
+            if (arr.length == 1) {
+                return new int[] {-1};
+            }
+
+            int min = Arrays.stream(arr)
+                    .min()
+                    .getAsInt();
+
+            return Arrays.stream(arr)
+                    .filter(num -> num != min)
+                    .toArray();
+        }
+
+        public int[] solution1(int[] arr) {
+            if (arr.length == 1) {
+                return new int[]{-1};
+            }
+
+            int[] answer = new int[arr.length - 1];
+            int minIndex = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[minIndex] > arr[i]) {
+                    minIndex = i;
+                }
+            }
+
+            for (int i = minIndex+1; i < arr.length; i++) {
+                arr[i - 1] = arr[i];
+            }
+
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = arr[i];
+            }
+
+            return answer;
+        }
+    }
+}
