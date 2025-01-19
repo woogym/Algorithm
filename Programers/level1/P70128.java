c8aad6f2182d5f9785669b04a49e9113755eb387
woogym <kwj9294@gmail.com>
2025-01-19 22:09:49 +0900

    p70128 - 내적

---
+++ Programers/level1/P70128.java
@@ -0,0 +1,16 @@
+package Algorithm.Programers.level1;
+
+// 내적
+public class P70128 {
+    class Solution {
+        public int solution(int[] a, int[] b) {
+            int answer = 0;
+
+            for (int i = 0; i < a.length; i++) {
+                answer += a[i] * b[i];
+            }
+
+            return answer;
+        }
+    }
+}
