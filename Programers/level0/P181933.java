dd75878919ee914ca2b81f51352f39ad33e2b9f4
woogym <kwj9294@gmail.com>
2025-01-24 23:16:07 +0900

    p181933

---
+++ Programers/level0/P181933.java
@@ -0,0 +1,13 @@
+package Algorithm.Programers.level0;
+
+public class P181933 {
+    class Solution {
+        public int solution(int a, int b, boolean flag) {
+            if (flag) {
+                return a + b;
+            } else {
+                return a - b;
+            }
+        }
+    }
+}
