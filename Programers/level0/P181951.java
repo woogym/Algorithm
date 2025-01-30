f83ed2aa2aa411bf3a74e2cab7976e65b5ec3705
woogym <kwj9294@gmail.com>
2025-01-31 01:28:03 +0900

    p181951

---
+++ Programers/level0/P181951.java
@@ -0,0 +1,15 @@
+package Algorithm.Programers.level0;
+import java.util.Scanner;
+
+public class P181951 {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+    }
+}
