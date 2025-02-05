90ae1ed6d79bcbb5b5354bc3deb7307e6e05cd33
woogym <kwj9294@gmail.com>
2025-02-06 01:47:34 +0900

    p181945

---
+++ Programers/level0/P181945.java
@@ -0,0 +1,16 @@
+package Algorithm.Programers.level0;
+
+import java.util.Scanner;
+
+public class P181945 {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+
+            for (int i = 0; i < a.length(); i++) {
+                System.out.println(a.charAt(i));
+            }
+        }
+    }
+}
