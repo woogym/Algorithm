41553d617b29b515f15a5835ad3cda98721fe2cb
woogym <kwj9294@gmail.com>
2025-03-05 11:12:25 +0900

    p181842

---
+++ Programers/level0/P181842.java
@@ -0,0 +1,9 @@
+package Algorithm.Programers.level0;
+
+public class P181842 {
+    class Solution {
+        public int solution(String str1, String str2) {
+            return str2.contains(str1) ? 1 : 0;
+        }
+    }
+}
