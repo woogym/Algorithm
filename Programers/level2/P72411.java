c16c79305fbdbf24a9f43a290feb8e9513d260b8
woogym <kwj9294@gmail.com>
2025-03-25 00:02:41 +0900

    p72411 - refactoring

--- Programers/level2/P72411.java
+++ Programers/level2/P72411.java
@@ -2,14 +2,29 @@ package Algorithm.Programers.level2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 // 메뉴 리뉴얼
 public class P72411 {
     class Solution {
 
         static HashMap<String, Integer> map;
+
+        public static void putCombination(String order, StringBuilder sb, int idx, int cnt, int n) {
+            if (cnt == n) {
+                map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+                return;
+            }
+
+            for (int i = idx; i < order.length(); i++) {
+                sb.append(order.charAt(i));
+                putCombination(order, sb, i + 1, cnt + 1, n);
+                sb.delete(cnt, cnt + 1);
+            }
+        }
 
         public String[] solution(String[] orders, int[] course) {
             List<String> answer = new ArrayList<>();
@@ -27,16 +42,25 @@ public class P72411 {
                 for (int j = 0; j < orders.length; j++) {
                     StringBuilder sb = new StringBuilder();
 
-                    if (course[i] <= orders[i].length()) {
+                    if (course[i] <= orders[j].length()) {
                         putCombination(orders[j], sb, 0, 0, course[i]);
+                    }
+                }
+
+                for (Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+                    max = Math.max(max, stringIntegerEntry.getValue());
+                }
+
+                for (Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+                    if (max >= 2 && stringIntegerEntry.getValue() == max) {
+                        answer.add(stringIntegerEntry.getKey());
                     }
                 }
             }
 
-            return answer.stream()
-                    .sorted()
-                    .toArray(String[]::new);
-        }
+            Collections.sort(answer);
 
+            return answer.toArray(String[]::new);
+        }
     }
 }
