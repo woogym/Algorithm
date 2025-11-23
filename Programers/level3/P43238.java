27ed6c4d13706c4e9ecf80c6ad64aa96b7f26094
woogym <kwj9294@gmail.com>
2025-11-23 21:46:24 +0900

    p43238 - 입국 심사 - 이분 탐색(복습ㄱㄱ)

---
+++ Programers/level3/P43238.java
@@ -0,0 +1,33 @@
+package Algorithm.Programers.level3;
+
+import java.util.Arrays;
+
+// 입국 심사 - 이중 탐색 문제
+public class P43238 {
+    class Solution {
+        public long solution(int n, int[] times) {
+            long answer = 0;
+            Arrays.sort(times);
+            long left = 0;
+            long right = (long) times[times.length - 1] * (long) n;
+
+            while (left <= right) {
+                long mid = (left + right) / 2;
+                long count = 0;
+
+                for (int i = 0; i < times.length; i++) {
+                    count += mid / times[i];
+                }
+
+                if (count < n) {
+                    left = mid + 1;
+                } else {
+                    answer = mid;
+                    right = mid - 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Programers/level3/Test.java
@@ -0,0 +1,12 @@
+package Algorithm.Programers.level3;
+
+import Algorithm.Programers.level3.P43238.Solution;
+
+public class Test {
+    public static void main(String[] args) {
+        P43238 p43238 = new P43238();
+        P43238.Solution solution = p43238.new Solution();
+
+        solution.
+    }
+}
